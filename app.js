d62e5ccbada247f7605740322a45cb88c562c07f
Adam Jamiu <adismail4@gmail.com>
2023-05-22 18:53:51 +0100

    Render all words meaning

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const inputWord = document.querySelector(".inputWord");
 const btn = document.querySelector(".btn");
 const details = document.querySelector(".details");
+const audio = document.querySelector(".audio");
 
 async function searchWord(word) {
   apiUrl = "https://api.dictionaryapi.dev/api/v2/entries/en/";
@@ -9,33 +10,29 @@ async function searchWord(word) {
   const [data] = await response.json();
   console.log(data);
 
-  const html = `<h3 class="word">${data.word} </h3>
-  <h3 class="word">${data.phonetic} </h3>
- 
+  const html = `
+  <div>
+    <h3 class="word">${data.phonetics[0].audio}</h3>
+    <h3 class="word">${data.word}</h3>
+    <h3 class="phonetic">${data.phonetic}</h3>    
+    
+    <div>
+      ${data.meanings
+        .map(
+          (m) =>
+            `<ol>${m.partOfSpeech}
+             ${m.definitions
+               .map((d) => `<li>${d.definition}</li>`)
+               .join("")}</ol>`
+        )
+        .join("")}
+    </div>
 
-  <p class="partOfSpeech">${data.meanings[0].partOfSpeech}</p>  
-  <ol>
-    ${data.meanings[0].definitions
-      .map((x) => `<li> ${x.definition}</li>`)
-      .join("")} 
-  </ol> 
-  <p class='synonyms'><span>synonyms</span> ${data.meanings[0].synonyms}</p>
-  <p class="partOfSpeech">${data.meanings[1].partOfSpeech}</p>  
-  <ol>
-  ${data.meanings[1].definitions
-    .map((x) => `<li> ${x.definition}</li>`)
-    .join("")} 
-</ol> 
-<p class="partOfSpeech">${data.meanings[2].partOfSpeech}</p>  
-  <ol>
-  ${data.meanings[2].definitions
-    .map((x) => `<li> ${x.definition}</li>`)
-    .join("")} 
-</ol> 
-`;
+    </div>
+
+  `;
 
   details.innerHTML = html;
-  inputWord.value = "";
 }
 
 btn.addEventListener("click", (e) => {
@@ -43,3 +40,7 @@ btn.addEventListener("click", (e) => {
   const wordValue = inputWord.value;
   searchWord(wordValue);
 });
+
+// audio.addEventListener('click', () => {
+//   audio.src = "https://api.dictionaryapi.dev/media/pronunciations/en/food-uk.mp3"
+// })
